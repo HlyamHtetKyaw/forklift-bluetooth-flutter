2f602d174850cc4903c29bb2f99cab33b889f649
hlyamhtetkyaw <hlan1559@gmail.com>
2025-08-17 00:22:01 +0630

    [ADD] arduino code

---
+++ ForkLiftArduinoSketch/ForkLiftArduinoSketch.ino
@@ -0,0 +1,117 @@
+#include <SoftwareSerial.h>
+#include <AFMotor.h>
+
+// ----------------- DC Motors -----------------
+AF_DCMotor motor1(1, MOTOR12_1KHZ); // Front Left
+AF_DCMotor motor2(2, MOTOR12_1KHZ); // Front Right
+AF_DCMotor motor3(3, MOTOR34_1KHZ); // Back Left
+AF_DCMotor motor4(4, MOTOR34_1KHZ); // Back Right
+int motorSpeed = 150;
+
+// Define SoftwareSerial pins for HC-05
+SoftwareSerial bluetooth(A0, A1); // RX, TX
+
+void setup() {
+  // Start the hardware serial (Serial Monitor)
+  Serial.begin(9600);
+
+  // Start software serial for HC-05
+  bluetooth.begin(9600);  // Default HC-05 baud rate
+
+  Serial.println("Ready. Type something in the Serial Monitor to send to HC-05");
+}
+
+void loop() {
+  if (bluetooth.available() > 0) {
+    char command = bluetooth.read();
+    Serial.println(command);
+    switch (command) {
+      case 'F': moveForward(); break;
+      case 'B': moveBackward(); break;
+      case 'L': moveLeft(); break;
+      case 'R': moveRight(); break;
+      case 'J': topLeft(); break;
+      case 'K': bottomright(); break;
+      case 'M': bottomleft(); break;
+      case 'I': topright(); break;
+      case 'T': stopMotors(); break;
+      // case 'S': startStepper(true, 200, 1000); break;   // CW
+      // case 's': startStepper(false, 200, 1000); break;  // CCW
+    }
+  }
+}
+
+void moveForward()  {
+  motor1.setSpeed(motorSpeed);
+  motor2.setSpeed(motorSpeed);
+  motor3.setSpeed(motorSpeed);
+  motor4.setSpeed(motorSpeed);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}
+void moveBackward() 
+  motor1.setSpeed(motorSpeed);
+  motor2.setSpeed(motorSpeed);
+  motor3.setSpeed(motorSpeed);
+  motor4.setSpeed(motorSpeed);
+  motor1.run(BACKWARD); 
+  motor2.run(BACKWARD); 
+  motor3.run(BACKWARD); 
+  motor4.run(BACKWARD); 
+}
+void moveLeft(){ 
+  motor1.setSpeed(motorSpeed); 
+  motor2.setSpeed(motorSpeed);
+  motor3.setSpeed(motorSpeed);
+  motor4.setSpeed(motorSpeed);
+  motor1.run(BACKWARD); 
+  motor2.run(FORWARD); 
+  motor3.run(FORWARD); 
+  motor4.run(BACKWARD); 
+}
+void moveRight(){ 
+  motor1.setSpeed(motorSpeed); 
+  motor2.setSpeed(motorSpeed); 
+  motor3.setSpeed(motorSpeed); 
+  motor4.setSpeed(motorSpeed);
+  motor1.run(FORWARD); 
+  motor2.run(BACKWARD); 
+  motor3.run(BACKWARD); 
+  motor4.run(FORWARD); 
+}
+void topLeft()
+{
+  motor1.setSpeed(Speed);
+  motor1.run(FORWARD);
+  motor3.setSpeed(Speed);
+  motor3.run(FORWARD);
+}
+void topRight()
+{
+  motor2.setSpeed(Speed);
+  motor2.run(BACKWARD);
+  motor4.setSpeed(Speed);
+  motor4.run(BACKWARD);
+}
+void bottomLeft()
+{
+  motor2.setSpeed(Speed);
+  motor2.run(FORWARD);
+  motor4.setSpeed(Speed);
+  motor4.run(FORWARD);
+}
+void bottomRight()
+{
+  motor1.setSpeed(Speed);
+  motor1.run(BACKWARD);
+  motor3.setSpeed(Speed);
+  motor3.run(BACKWARD);
+}
+void stopMotors(){
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+}
